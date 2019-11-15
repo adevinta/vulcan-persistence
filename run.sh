e819f533e54c0b5db68cee8b452a84f51ff30137
Jesus-Carpintero <jesus.carpintero@adevinta.com>
2019-11-15 14:27:57 +0100

    Allow to execute an sql import

--- run.sh
+++ run.sh
@@ -4,4 +4,10 @@ cp $1 .env.production
 unset VERSION # unset VERSION to prevent conflicts in db:migrate
 bin/rails db:migrate
 
+if [ -f "$2" ]
+then
+  source .env.production
+  PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -1 -f $2
+fi
+
 bundle exec puma -C config/puma.rb

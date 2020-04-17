3c562804238974efb104a218d47670a8a86c543c
Daniel Beneyto <danfaizer@gmail.com>
2020-04-17 14:13:07 +0200

    Refactor AWS client initialization

--- config/initializers/aws_clients.rb
+++ config/initializers/aws_clients.rb
@@ -5,63 +5,62 @@ s3_endpoint = ENV['AWS_S3_ENDPOINT'] || ""
 sns_endpoint = ENV['AWS_SNS_ENDPOINT'] || ""
 sqs_endpoint = ENV['AWS_SQS_ENDPOINT'] || ""
 
+Aws.config.update({
+  retry_backoff: lambda { |c| sleep(3) }
+})
+
 if region != ""
   Aws.config.update({
     region: region,
   })
 end
 
+sqs_options = {}
+sns_options = {}
+s3_options  = {}
+
 if ENV['RAILS_ENV'] == "test"
   WebMock.disable_net_connect!(allow_localhost: true)
-  # SQS Client
-  Rails.application.config.sqs_client = Aws::SQS::Client.new(
-    stub_responses: true,
-  )
-  Rails.application.config.sqs_client.stub_responses(
-    :get_queue_url, { queue_url: 'http://localhost/dummy' },
-  )
-  Rails.application.config.sqs_client.stub_responses(
-    :send_message, { message_id: "fake_message_id" },
-  )
-  # SNS Client
-  Rails.application.config.sns_client = Aws::SNS::Client.new(
-    stub_responses: true,
-  )
-  Rails.application.config.sns_client.stub_responses(
-    :publish, {},
-  )
-  # S3 Client
-  Rails.application.config.s3_client = Aws::S3::Client.new(
-    stub_responses: true,
-  )
-# Not testing, create not mocked clients.
-else
-  # SQS Client
-  if sqs_endpoint != ""
-    Rails.application.config.sqs_client = Aws::SQS::Client.new(
-      :endpoint => sqs_endpoint,
-      :log_level => "debug",
-    )
-  else
-    Rails.application.config.sqs_client = Aws::SQS::Client.new()
-  end
-  # SNS Client
-  if sns_endpoint != ""
-    Rails.application.config.sns_client = Aws::SNS::Client.new(
-      :endpoint => sns_endpoint,
-      :log_level => "debug",
-    )
-  else
-    Rails.application.config.sns_client = Aws::SNS::Client.new()
-  end
-  # S3 Client
-  if s3_endpoint != ""
-    Rails.application.config.s3_client = Aws::S3::Client.new(
-      :endpoint => s3_endpoint,
-      :force_path_style => true,
-      :log_level => "debug",
-    )
-  else
-    Rails.application.config.s3_client = Aws::S3::Client.new()
-  end
+  sqs_options = {
+    :stub_responses  => {
+      :get_queue_url => { queue_url:  'http://localhost/dummy' },
+      :send_message  => { message_id: 'fake_message_id' },
+    }
+  }
+
+  sns_options = {
+    :stub_responses => {
+      :publish      => {},
+    }
+  }
+
+  s3_options = {
+    :stub_responses => true,
+  }
 end
+
+sqs_options.merge(sqs_endpoint != "" ?
+  {
+    :endpoint  => sqs_endpoint,
+    :log_level => "debug",
+  } : {}
+)
+
+sns_options.merge(sns_endpoint != "" ?
+  {
+    :endpoint  => sns_endpoint,
+    :log_level => "debug",
+  } : {}
+)
+
+s3_options.merge(s3_endpoint != "" ?
+  {
+    :endpoint         => s3_endpoint,
+    :force_path_style => true,
+    :log_level        => "debug",
+  } : {}
+)
+
+Rails.application.config.sqs_client = Aws::SQS::Client.new(sqs_options)
+Rails.application.config.sns_client = Aws::SNS::Client.new(sns_options)
+Rails.application.config.s3_client  = Aws::S3::Client.new(s3_options)

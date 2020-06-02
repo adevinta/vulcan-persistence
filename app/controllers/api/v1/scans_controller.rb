83c09849921909f135cf741c45ab24356e7fd98f
Daniel Beneyto <danfaizer@gmail.com>
2020-06-02 16:01:53 +0200

    Add metrics for scan

--- app/controllers/api/v1/scans_controller.rb
+++ app/controllers/api/v1/scans_controller.rb
@@ -41,6 +41,7 @@ module Api::V1
 
       if @scan.save
         render json: @scan, status: :created, location: [:v1, @scan]
+        push_scan_metrics if Rails.application.config.metrics
       else
         render json: @scan.errors, status: :unprocessable_entity
         return
@@ -93,6 +94,28 @@ module Api::V1
     # Notifies action to stream
     def notify(action)
       NotificationsHelper.notify(action: action, scan_id: @scan.id)
+    end
+
+    def push_scan_metrics
+      scan_label = ""
+      team_label = ""
+      begin
+        # obtain info from scan (program name)
+        scan_label = @scan[:program_name].downcase || "unknown-program"
+      rescue
+        Rails.logger.warn "error obtaining program name for scan [#{@scan.id}] for pushing metrics"
+        scan_label = "unknown-program"
+      end
+      begin
+        team_label = @scan[:tag].split(':').last.downcase || "unknown-team"
+      rescue
+        Rails.logger.warn "error obtaining team name for scan [#{@scan.id}] for pushing metrics"
+        team_label = "unknown-team"
+      end
+
+      # metric_tags = ["scan:#{team_label}-#{scan_label}"]
+      metric_tags = ["scan:purple-periodic-full-scan"]
+      Metrics.count("scan.count", 1, metric_tags)
     end
   end
 end

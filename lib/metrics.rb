3b95ad5c7f08e8911e0ad71fc1c786bc29d28e7c
Daniel Beneyto <danfaizer@gmail.com>
2020-06-02 15:56:28 +0200

    Add count metric method

--- lib/metrics.rb
+++ lib/metrics.rb
@@ -1,4 +1,17 @@
 class Metrics
+  def self.count(metric_name, metric_value, metric_tags)
+    begin
+      Rails.application.config.statsd.count(
+        metric_name,
+        metric_value,
+        tags: Rails.application.config.metric_tags + metric_tags
+      )
+      Rails.logger.debug "pushed count metric [#{metric_name}] with tags [#{metric_tags}]"
+    rescue
+      Rails.logger.warn "error pushing count metric [#{metric_name}] with tags [#{metric_tags}]"
+    end
+  end
+
   def self.increment(metric_name, metric_tags)
     begin
       Rails.application.config.statsd.increment(

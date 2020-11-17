b7549a716b99416fd07c0f740fe439aeda2b6767
Manel Montilla <manel.montilla@schibsted.com>
2020-11-17 15:52:18 +0100

    OOO check status uptade and check, scan ID

--- app/models/check.rb
+++ app/models/check.rb
@@ -1,13 +1,19 @@
 class Check < ApplicationRecord
+  @@allowed_trasitions = {
+    :CREATED => [:QUEUED, :ASSIGNED, :MALFORMED, :RUNNING, :FAILED, :FINISHED, :ABORTED,:PURGING, :KILLED, :TIMEOUT],
+    :QUEUED => [:ASSIGNED, :MALFORMED, :RUNNING, :FAILED, :FINISHED, :ABORTED,:PURGING, :KILLED, :TIMEOUT],
+    :ASSIGNED => [:MALFORMED, :RUNNING, :FAILED, :FINISHED, :ABORTED,:PURGING, :KILLED, :TIMEOUT],
+    :RUNNING => [:MALFORMED, :FAILED, :FINISHED, :ABORTED,:PURGING, :KILLED, :TIMEOUT]
+  }
+  @@terminal_statuses= [:MALFORMED, :ABORTED, :KILLED, :FAILED, :FINISHED, :TIMEOUT, :INCONCLUSIVE]
   # checktype_name and metadata are virtual attributes, not persisted in the model.
   attr_accessor :checktype_name
   attr_accessor :metadata
 
   include Filterable
-  include AASM
 
   after_commit :sns_publish
-  before_update :verify_state_machine
+  before_update :verify_transition
   # In case the transaction is rolled back, we might had push an incorrect metric.
   # Proper hook to push the metric would be after_commit, but then is harder to
   # detect if status has changed. This is the best compromise in effort and consistency.
@@ -23,73 +29,24 @@ class Check < ApplicationRecord
   scope :target, -> (target) { where target: target }
   scope :checktype_id, -> (checktype_id) { where checktype_id: checktype_id }
   scope :agent_id, -> (agent_id) { where agent_id: agent_id }
-
-  aasm :column => :status do
-    state :CREATED, :initial => true
-    state :QUEUED
-    state :ASSIGNED
-    state :MALFORMED
-    state :RUNNING
-    state :FAILED
-    state :FINISHED
-    state :ABORTED
-    state :PURGING
-    state :KILLED
-    state :TIMEOUT
-    state :INCONCLUSIVE
-
-    # Enqueue is the event of a check being added to the queue.
-    event :enqueue do
-      transitions :from => :CREATED, :to => :QUEUED
-    end
-    # Assign is the event of an agent taking ownership of a check.
-    event :assign do
-      transitions :from => :QUEUED, :to => :ASSIGNED
-    end
-    # Malform is the event of an agent marking a check job as malformed.
-    event :malform do
-      transitions :from => :ASSIGNED, :to => :MALFORMED
-    end
-    # Timeout is the event of a check that has timed out.
-    event :timeout do
-      transitions :from => :RUNNING, :to => :TIMEOUT
-    end
-    # Run is the event of an agent running a check in its runtime environment.
-    event :run do
-      transitions :from => :ASSIGNED, :to => :RUNNING
-    end
-    # Abort is the event of confirming the abortion of a check.
-    event :abort do
-      transitions :from => [:CREATED , :ASSIGNED, :RUNNING], :to => :ABORTED
-    end
-    # Purge is the event of a check being flagged to be killed.
-    # This event is ment to be triggered by a janitor service.
-    event :purge do
-      transitions :from => :RUNNING, :to => :PURGING
-    end
-    # Kill is the event of an agent confirming that a check is killed.
-    # This can happen because a check was scheduled to be purged,
-    # because the check could not be successfully aborted cleanly or
-    # because the agent has disconnected and is purging all running checks.
-    event :kill do
-      transitions :from => [:RUNNING, :PURGING], :to => :KILLED
-    end
-    # Fail is the event of a check failing for any reason.
-    event :fail do
-      transitions :from => [:ASSIGNED, :RUNNING], :to => :FAILED
-    end
-    # Finish is the event of a check finishing successfully.
-    event :finish do
-      transitions :from => :RUNNING, :to => :FINISHED
-    end
-    # Inconclusive is the event of a check ending in an inconclusive state.
-    event :inconclusive do
-      transitions :from => :RUNNING, :to => :INCONCLUSIVE
-    end
-  end
-
   def sns_publish()
     SNSPublishJob.perform_later(self)
+  end
+
+  def abort!
+    self[:status] = "ABORTED"
+    allowed = verify_transition
+    return false unless allowed
+
+    save
+  end
+
+  def purge!
+    self[:status] = "PURGING"
+    allowed = verify_transition
+    return false unless allowed
+
+    save
   end
 
   private
@@ -130,36 +87,21 @@ class Check < ApplicationRecord
     Metrics.count("scan.check.count", 1, metric_tags)
   end
 
-  def verify_state_machine
-    if status
-      db_check = Check.find(id)
-      # There is a status change. Need to check state machine
-      if db_check[:status] != self[:status]
-        case self[:status]
-        when "QUEUED"
-          db_check.enqueue
-        when "ASSIGNED"
-          db_check.assign
-        when "MALFORMED"
-          db_check.malform
-        when "RUNNING"
-          db_check.run
-        when "TIMEOUT"
-          db_check.timeout
-        when "FAILED"
-          db_check.fail
-        when "ABORTED"
-          db_check.abort
-        when "PURGING"
-          db_check.purge
-        when "KILLED"
-          db_check.kill
-        when "FINISHED"
-          db_check.finish
-        when "INCONCLUSIVE"
-          db_check.inconclusive
-        end
-      end
-    end
+  def verify_transition
+    return true unless status
+    db_check = Check.find(id)
+    # We do not allow status changes if the check is already finished.
+    return false if @@terminal_statuses.include?(db_check[:status].intern)
+    # Check if the status change is allowed.
+    # It will be only if the new estatus is "greater" than the current one.
+    return true if db_check[:status] == self[:status]
+    return true if @@terminal_statuses.include?(self[:status].intern)
+
+    allowed = @@allowed_trasitions[db_check[:status].intern]
+
+    return false unless allowed
+    return false unless allowed.include?(self[:status].intern)
+
+    return true
   end
 end

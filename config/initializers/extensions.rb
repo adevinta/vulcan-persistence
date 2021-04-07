af569049702704b51283aebcd125e81cc6b6c4ec
Daniel Jimenez <daniel.jimenez@adevinta.com>
2021-04-07 11:45:50 +0200

    Revert "Remove jobqueues controller"
    This reverts commit 3c475cbfbf04406cb405afdf468c659c9855e665.

--- config/initializers/extensions.rb
+++ config/initializers/extensions.rb
@@ -1,3 +1,4 @@
 # Copyright 2019 Adevinta
 
+require "#{Rails.root}/lib/jobqueues_helper.rb"
 require "#{Rails.root}/lib/metrics.rb"
---
+++ test/models/jobqueue_test.rb
@@ -0,0 +1,9 @@
+# Copyright 2019 Adevinta
+
+require 'test_helper'
+
+class JobqueueTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

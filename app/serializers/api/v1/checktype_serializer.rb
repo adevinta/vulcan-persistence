b58a52303c8c641697c065edc0ef21086dae4059
Daniel Beneyto <danfaizer@gmail.com>
2021-04-12 15:11:00 +0200

    Remove queue_name from checktype serializer

--- app/serializers/api/v1/checktype_serializer.rb
+++ app/serializers/api/v1/checktype_serializer.rb
@@ -4,7 +4,7 @@ module Api::V1
   class ChecktypeSerializer < ActiveModel::Serializer
     include Rails.application.routes.url_helpers
 
-    attributes :id, :name, :description, :timeout, :enabled, :options, :required_vars, :image, :assets, :queue_name
+    attributes :id, :name, :description, :timeout, :enabled, :options, :required_vars, :image, :assets
 
     attribute :links do
       { self: v1_checktype_path(object.id) }

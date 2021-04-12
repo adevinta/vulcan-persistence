abfd2d8042b78d8fa3648499186462bda11f1cef
Daniel Beneyto <danfaizer@gmail.com>
2021-04-12 15:07:08 +0200

    Remove jobqueue controller reference in migration

--- db/migrate/20190325142830_add_name_to_jobqueues.rb
+++ db/migrate/20190325142830_add_name_to_jobqueues.rb
@@ -4,12 +4,5 @@ class AddNameToJobqueues < ActiveRecord::Migration[5.0]
   def change
     add_column :jobqueues, :name, :string, null: true
     add_index :jobqueues, :name
-
-    # Populate queue name with queue arn logical name
-    Jobqueue.find_each do |jq|
-      jobqueue_name = jq.arn.split(':').last
-      jq.name = jobqueue_name
-      jq.save
-    end
   end
 end

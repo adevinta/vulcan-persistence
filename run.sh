736ffb373e3a414dd81a498b1514cfa3c514d6fe
Jesus Carpintero <jesus.carpintero@adevinta.com>
2021-10-20 14:56:46 +0200

    Use postgres 13

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 # Copyright 2019 Adevinta
 cat .env.config | envsubst > .env.production
 
-if [ ! -z "$POSTGRES_CA_B64" ]; then
+if [ -n "$POSTGRES_CA_B64" ]; then
   mkdir -p /root/.postgresql
-  echo $POSTGRES_CA_B64 | base64 -d > /root/.postgresql/root.crt  # for rails
+  echo "$POSTGRES_CA_B64" | base64 -d > /root/.postgresql/root.crt  # for rails
 fi
 
 unset VERSION # unset VERSION to prevent conflicts in db:migrate
@@ -14,7 +14,7 @@ bundle exec rake db:migrate
 if [ -f "$1" ]
 then
   source .env.production
-  PGPASSWORD=$POSTGRES_PASSWORD PGSSLMODE=$POSTGRES_SSLMODE psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -1 -f $1
+  PGPASSWORD=$POSTGRES_PASSWORD PGSSLMODE=$POSTGRES_SSLMODE psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -1 -f "$1"
 fi
 
 bundle exec puma -C config/puma.rb

ded3d01d86f253d73749de552491f63bf5f5f67b
Daniel Beneyto <danfaizer@gmail.com>
2022-11-04 17:44:40 +0100

    Set cache-control header for assettypes endpoint

--- app/controllers/api/v1/assettypes_controller.rb
+++ app/controllers/api/v1/assettypes_controller.rb
@@ -4,6 +4,7 @@ module Api::V1
   class AssettypesController < ApplicationController
     # GET /assettypes
     def index
+      expires_in 2.minutes, public: true
       @assettypes = Assettype.all
       render json: @assettypes
     end
